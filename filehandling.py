f86c47fca98604283c80d568d669c0166f9a29a9
Cassian Celsus <125782348+CelsusK@users.noreply.github.com>
2025-04-09 09:33:55 +0300

    Create filehandling.py

---
+++ filehandling.py
@@ -0,0 +1,11 @@
+try:
+  with open("names.txt", "r+") as file:
+    Name = file.read()
+    print(Name)
+    file.write("JoNas")
+    print("New name added, Jonas")
+except FileNotFoundError:
+  print("File not found.Check the filename")
+finally:
+  file.close()
+  
