5c896653f1e750c2c558a7a8b43904ef8d081bc1
CelsusK <kiragucassian@gmail.com>
2025-04-16 10:35:01 +0300

    Add

---
+++ pythonplp/animove.py
@@ -0,0 +1,18 @@
+class Animal:
+    def move(self):
+        print("Moving...")
+class snake:
+    def move(self):
+        print("slithering")
+class horse:
+    def move(self):
+        print("galloping")
+class fish:
+    def move(self):
+        print("swimming")
+Snake = snake()
+print(Snake.move())
+Horse = horse() 
+print(Horse.move())
+Fish = fish()
+print(Fish.move())
